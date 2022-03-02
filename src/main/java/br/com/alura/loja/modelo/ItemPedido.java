eec921ddb9c097bb3a5c307cc6493bfc0b15d7ab
r-afael <roliveira1111@gmail.com>
2022-03-01 23:16:10 -0300

    Criando relatorio de vendas e value object

--- src/main/java/br/com/alura/loja/modelo/ItemPedido.java
+++ src/main/java/br/com/alura/loja/modelo/ItemPedido.java
@@ -78,6 +78,10 @@ public class ItemPedido {
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
+
+	public BigDecimal getValor() {
+		return precoUnitario.multiply(new BigDecimal(quantidade));
+	}
 	
 	
 }
